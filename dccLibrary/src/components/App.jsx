b7b4e0cd8c5bf790dc82f3dbbc8f8e564498be64
Dillon Coleman <coleman399@gmail.com>
2021-11-12 16:27:15 -0700

    centered book display on page

--- dccLibrary/src/components/App.jsx
+++ dccLibrary/src/components/App.jsx
@@ -14,23 +14,37 @@ class App extends Component {
         this.state = {
             bookNumber: 0,
         };
+    }
 
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+
+        this.setState({
+            bookNumber: tempBookNumber
+        });
     }
 
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
-                <div className="col-md-4">
-                    {/*Button here to move to the previous book viewed*/}
-                </div>
-                <div className="col-md-4">
-                    {/*Display Book with cover here*/}
-                    <h1>{this.books[this.state.bookNumber].title}</h1>
-                    <h4>{this.books[this.state.bookNumber].author}</h4>
-                </div>
-                <div className="col-md-4">
-                    {/*Button here to move to the next book viewed*/}
+                <div className="row">
+                    <div className="col-md-4">
+                        {/*Button here to move to the previous book viewed*/}
+                    </div>
+                    <div className="col-md-4">
+                        {/*Display Book with cover here*/}
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col-md-4">
+                        {/*Button here to move to the next book viewed*/}
+                        <button onClick={this.goToNextBook}>Next Book</button>
+                    </div>
                 </div>
             </div>
         )
