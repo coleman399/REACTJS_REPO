8b4ab181d6fb834ce39970916b05495b21d7814c
Dillon Coleman <coleman399@gmail.com>
2021-11-12 17:23:53 -0700

    Serparated Book View Display Into Separate Book Component

--- dccLibrary/src/components/BookViewer/BookViewer.jsx
+++ dccLibrary/src/components/BookViewer/BookViewer.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
+import Book from '../Book/Book';
 
 const BookViewer = (props) => {
     return (
         <div className="row row-spacer">
             <div className="col-md-4">
-                {/*Button here to move to the previous book viewed*/}
                 <button onClick={props.previousBook}>Previous Book</button>
             </div>
             <div className="col-md-4">
                 {/*Display Book with cover here*/}
-                <h1>{props.book.title}</h1>
-                <h4>{props.book.author}</h4>
+                <Book book={props.book}/>
             </div>
             <div className="col-md-4">
-                {/*Button here to move to the next book viewed*/}
                 <button onClick={props.goToNextBook}>Next Book</button>
             </div>  
         </div>
